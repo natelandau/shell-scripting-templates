62d0e09f4e3a0f283dc86d092f1e134884a174ba
Nathaniel Landau <nate@natelandau.com>
2015-05-04 08:22:51 -0400

    added new packages to FFMPEG

--- setupScripts/homebrew.sh
+++ setupScripts/homebrew.sh
@@ -153,7 +153,7 @@ install-ffmpeg () {
   if type_not_exists 'ffmpeg'; then
     seek_confirmation "Install ffmpeg?"
     if is_confirmed; then
-      brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-libcaca --with-libass --with-frei0r --with-libquvi --with-libvidstab --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-openssl --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools --with-x265
+      brew install ffmpeg --with-faac --with-fdk-aac --with-ffplay --with-fontconfig --with-freetype --with-libcaca --with-libass --with-frei0r --with-libass --with-libbluray --with-libcaca --with-libquvi --with-libvidstab --with-libsoxr --with-libssh --with-libvo-aacenc --with-libvidstab --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-openssl --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools --with-webp --with-x265
     fi
   fi
 }
