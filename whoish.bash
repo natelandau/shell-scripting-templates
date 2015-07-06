dc6daa20611c914c4f6e4b32a8905639a70e9a88
A. River <ariver@runsthruit.com>
2015-07-06 07:47:27 -0500

    WHOIS via curl calls.

---
+++ whoish.bash
@@ -0,0 +1,34 @@
+
+function whoish ()
+{
+    #
+    declare addr curl_cmd json jq_flt urls url
+    #
+    addr="${1}"
+    printf -v jq_flt %s \
+        '.nets.net' \
+        ' | ' \
+            'if ( . | type ) == "object" then' \
+                ' . ' \
+            'elif ( . | type ) == "array" then' \
+                ' .[] ' \
+            'else' \
+                ' "ERR" ' \
+            'end' \
+            ' | ' \
+                '.ref["$"] , .orgRef["$"]'
+    curl_cmd=(
+        curl -s
+        -H 'Accept: application/json'
+        "http://whois.arin.net/rest/nets;q=${addr}?showDetails=true"
+    )
+    #
+    json="$( "${curl_cmd[@]}" )"
+    #
+    urls=( $( echo "${json}" | jq "${jq_flt}" ) )
+    #
+    curl -s -H 'Accept: text/plain' "${urls[@]//\"/}" |
+        grep -v '^#' |
+        uniq
+    #
+}
