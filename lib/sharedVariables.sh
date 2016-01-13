6382d987391b66765e4251d7d3307a977f8261dc
Nathaniel Landau <nate@natelandau.com>
2016-01-13 10:57:57 -0500

    added

--- lib/sharedVariables.sh
+++ lib/sharedVariables.sh
@@ -10,6 +10,7 @@
 # * 2015-01-02 - v1.0.0 - First Creation
 # * 2015-08-05 - v1.0.1 - Now has $hourstamp (10:34:40 PM)
 # * 2016-01-10 - v1.0.2 - Now has $longdate and set LC_ALL=C to all dates
+# * 2016-01-13 - v1.0.3 - now has $gmtdate
 #
 # ##################################################
 
@@ -31,6 +32,7 @@ hourstamp=$(LC_ALL=C date +%r)             # Returns: 10:34:40 PM
 timestamp=$(LC_ALL=C date +%Y%m%d_%H%M%S)  # Returns: 20150614_223440
 today=$(LC_ALL=C date +"%m-%d-%Y")         # Returns: 06-14-2015
 longdate=$(LC_ALL=C date +"%a, %d %b %Y %H:%M:%S %z")  # Returns: Sun, 10 Jan 2016 20:47:53 -0500
+gmtdate=$(LC_ALL=C date -u -R | sed 's/\+0000/GMT/') # Returns: Wed, 13 Jan 2016 15:55:29 GMT
 
 # THISHOST
 # ------------------------------------------------------
