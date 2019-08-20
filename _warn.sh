1f1aa376b505911d1391f0679f450cba7ea528cf
Joe Mooring <joe.mooring@veriphor.com>
2019-08-20 12:22:03 -0400

    Require message when calling bfl::warn()

--- _warn.sh
+++ _warn.sh
@@ -9,7 +9,7 @@
 # @function
 # Prints warning message to stdout.
 #
-# @param string $msg (optional)
+# @param string $msg
 #   The warning message.
 #
 # @example
@@ -19,10 +19,13 @@
 #------------------------------------------------------------------------------
 bfl::warn() {
   # Verify argument count.
-  bfl::verify_arg_count "$#" 0 1 || exit 1
+  bfl::verify_arg_count "$#" 1 1 || exit 1
 
   # Declare positional arguments (readonly, sorted by position).
-  declare -r msg="${1:-"Warning: unspecified warning."}"
+  declare msg="${1}"
+
+  # Verify argument values.
+  bfl::is_blank "$msg" && bfl::die "Error: warning message not specified."
 
   # Print the message.
   printf "%b\\n" "${yellow}${msg}${reset}"
